dd7857b075d2e56a4b48e5f086a59421176e8062
unknown <sushantvichare10@gmail.com>
2024-10-16 16:02:26 +0530

    create user api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Connected to Database");
@@ -10,6 +11,8 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/my/user", myUserRoute);
 
 app.get("/test", async (req: Request, res: Response) => {
   res.json({ message: "Hello, world!" });
