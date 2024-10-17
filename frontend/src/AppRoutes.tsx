8eee9e062e08116f0b4398fb4e304a836e0d289c
unknown <sushantvichare10@gmail.com>
2024-10-17 14:09:03 +0530

    Auth0 access token

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./Pages/HomePage";
+import AuthCallbackPage from "./Pages/AuthCallbackPage";
 
 const AppRoutes = () => {
   return (
@@ -13,6 +14,7 @@ const AppRoutes = () => {
           </Layout>
         }
       />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
       <Route path="/user-profile" element={<span>User Profile</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
